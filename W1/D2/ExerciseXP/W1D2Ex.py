b4a4f8b2d09d9df5f68dc32e60f3201df5feb3d7
IVSpektor <spektorilia@gmail.com>
2025-05-12 13:37:00 +0300

    w1d1v1

---
+++ W1/D2/ExerciseXP/W1D2Ex.py
@@ -0,0 +1,20 @@
+# Exercise 1: Favorite Numbers
+
+# my_fav_numbers = {1, 3, 5, 7}
+# print(my_fav_numbers)
+# my_fav_numbers.add(4)
+# my_fav_numbers.add(6)
+# print(my_fav_numbers)
+# my_fav_numbers.remove(6)
+# print(my_fav_numbers)
+# friend_fav_numbers = {2,3,4}
+# our_fav_numbers = my_fav_numbers.intersection(friend_fav_numbers)
+# print(our_fav_numbers)
+
+# Exercise 2: Tuple
+
+tuple_test = (1, 2, 3)
+print(tuple_test)
+print(type(tuple_test))
+tuple_test = tuple_test + tuple(4, 5)
+print(tuple_test)
