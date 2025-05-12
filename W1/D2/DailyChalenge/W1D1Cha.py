7b86b8772fa5f04a20f03e19b393a7686e948f32
IVSpektor <spektorilia@gmail.com>
2025-05-12 18:20:16 +0300

    w1d1v3

---
+++ W1/D2/DailyChalenge/W1D1Cha.py
@@ -0,0 +1,21 @@
+# Challenge 1: Multiples of a Number
+# first_number = int(input("Input the first number: "))
+# length_list = int(input("Input length of list: "))
+# list_numbers = []
+#
+# for num in range(1, length_list + 1):
+#     list_numbers.append(first_number*num)
+#
+# print(list_numbers)
+
+# Challenge 2: Remove Consecutive Duplicate Letters
+# users_string = input("Input string: ")
+# result_string = ""
+# index = 0
+# while index < len(users_string):
+#     result_string += users_string[index]
+#     while index+1 < len(users_string) and users_string[index] == users_string[index+1]:
+#         index += 1
+#     index += 1
+# 
+# print(result_string)
