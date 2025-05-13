f700c98503beed7941d8fc902dc342f2a06c654a
IVSpektor <spektorilia@gmail.com>
2025-05-13 16:26:25 +0300

    w1d3v2

---
+++ W1/D3/DailyChalenge/W1D3Cha.py
@@ -0,0 +1,26 @@
+# Challenge 1: Letter Index Dictionary
+# user_word = input("Please input the word: ")
+# final_dict = {}
+# for i, char in enumerate(user_word):
+#     if final_dict.get(char):
+#         final_dict[char].append(i)
+#     else:
+#         final_dict[char] = [i]
+# print(final_dict)
+
+# Challenge 2: Affordable Items
+# items_purchase = {"Apple": "$4", "Honey": "$3", "Fan": "$14", "Bananas": "$4", "Pan": "$100", "Spoon": "$2"}
+# wallet = "$100"
+# money = int(wallet.replace("$", ""))
+# list_name = []
+#
+# for name, cost in items_purchase.items():
+#     new_cost = int(cost.replace("$", "").replace(",", ""))
+#     if money > new_cost:
+#         list_name.append(name)
+#         money -= new_cost
+# if len(list_name) > 0:
+#     print(sorted(list_name))
+# else:
+#     print("Nothing")
+
