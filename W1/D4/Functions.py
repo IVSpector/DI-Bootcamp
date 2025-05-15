f934c0c53fa2714690a4b0f2b74a841546e4ef57
IVSpektor <spektorilia@gmail.com>
2025-05-15 17:16:21 +0300

    w1d5v1

--- W1/D4/Functions.py
+++ W1/D4/Functions.py
@@ -76,17 +76,36 @@
 # Global scope: on the file, in general
 # Local scope: idented block (if statement, a for loop, a function...)
 
+#
+# global_var = 100
+# def calculation(a, b):
+#     addition = a + b
+#     substraction = a - b
+#     # global_var = 50
+#     global global_var
+#     global_var = 70
+#     return addition, substraction
+#
+#
+# addition, substract = calculation(5, 7)
+# # print(addition, substract)
+# print(global_var)
+#
+#
+# def decode_matrix_string(matrix_str: str) -> str:
+#     matrix = [list(line) for line in matrix_str.strip().split('\n')]
+#     max_cols = max(len(row) for row in matrix)
+#     chars = []
+#     for col in range(max_cols):
+#         for row in range(len(matrix)):
+#             if col < len(matrix[row]):
+#                 char = matrix[row][col]
+#                 chars.append(char if char.isalpha() else " ")
+#     return " ".join("".join(chars).split())
+#
+#
+# string = " \n     asdf \n 123 qwe 567 ghj"
+# matrix = [list(line) for line in string.strip().split("\n")]
+# print(matrix)
 
-global_var = 100
-def calculation(a, b):
-    addition = a + b
-    substraction = a - b
-    # global_var = 50
-    global global_var
-    global_var = 70
-    return addition, substraction
 
-
-addition, substract = calculation(5, 7)
-# print(addition, substract)
-print(global_var)
